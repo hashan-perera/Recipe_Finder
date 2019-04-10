2870c8e9a849e5d76555bbc8c23dde15d09833e3
Hashantha Perera <7hashd@gmail.com>
2019-04-10 12:04:18 +0530

    db

--- app/src/main/java/com/example/recipe_app/DatabaseHelper.java
+++ app/src/main/java/com/example/recipe_app/DatabaseHelper.java
@@ -1,4 +1,45 @@
 package com.example.recipe_app;
 
-public class DatabaseHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String db_name = "products.db";
+    public static final String table_name = "products_table";
+    public static final String col_1 = "NAME";
+    public static final String col_2 = "WEIGHT";
+    public static final String col_3 = "PRICE";
+    public static final String col_4 = "DESCRIPTION";
+
+    public DatabaseHelper(Context context) {
+        super(context, db_name, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + table_name + "(NAME TEXT,WEIGHT DOUBLE, PRICE DOUBLE, DESCRIPTION TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(" DROP TABLE IF EXISTS " + table_name);
+        onCreate(db);
+    }
+
+    public boolean insertData(String name, String weight, String price, String description) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(col_1, name);
+        contentValues.put(col_2, weight);
+        contentValues.put(col_3, price);
+        contentValues.put(col_4, description);
+        long result = db.insert(table_name, null, contentValues);
+        if (result == -1)
+            return false;
+        else
+            return true;
+    }
 }
--- app/src/main/java/com/example/recipe_app/RegisterProduct.java
+++ app/src/main/java/com/example/recipe_app/RegisterProduct.java
@@ -2,20 +2,48 @@ package com.example.recipe_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterProduct extends AppCompatActivity {
+
+    EditText name, weight, price, description;
+    Button save;
+    DatabaseHelper mydb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_product);
 
-        EditText name = findViewById(R.id.name);
-        EditText weight = findViewById(R.id.weight);
-        EditText price = findViewById(R.id.price);
-        EditText description = findViewById(R.id.description);
-        Button save = findViewById(R.id.save);
+        name = findViewById(R.id.name);
+        weight = findViewById(R.id.weight);
+        price = findViewById(R.id.price);
+        description = findViewById(R.id.description);
+        save = findViewById(R.id.save);
+        mydb = new DatabaseHelper(this);
+
+        //
+        AddData();
+
+    }
+
+    public void AddData() {
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isInserted = mydb.insertData(
+                        name.getText().toString(),
+                        weight.getText().toString(),
+                        price.getText().toString(),
+                        description.getText().toString());
+                if (isInserted == true)
+                    Toast.makeText(RegisterProduct.this, "Added to Database", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(RegisterProduct.this, "Error!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
